17d47ee730d95d4a77597b7f42a87b9dbbf562df
deepak-subedi <deepak.subedi311@gmail.com>
2026-02-28 10:42:30 +0900

    first commit

---
+++ models/marts/core/dim_products.sql
@@ -0,0 +1,15 @@
+with products as (
+    select * from {{ ref('stg_products') }}
+),
+
+final as (
+    select
+        product_id,
+        product_name,
+        category,
+        price,
+        {{ dbt_utils.generate_surrogate_key(['product_id']) }} as product_key
+    from products
+)
+
+select * from final
---
+++ models/staging/stg_order_items.sql
@@ -0,0 +1,15 @@
+with source as (
+    select * from {{ ref('raw_order_items') }}
+),
+
+renamed as (
+    select
+        order_item_id,
+        order_id,
+        product_id,
+        cast(quantity as int) as quantity,
+        cast(unit_price as decimal(12, 2)) as unit_price
+    from source
+)
+
+select * from renamed
